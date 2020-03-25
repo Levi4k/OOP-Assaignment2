0f1e876bbcd02879ecd058e5cc99fda739788174
Levi4k <60514442+Levi4k@users.noreply.github.com>
2020-03-25 10:57:38 +0000

    Add files via upload

---
+++ OOP assaignement 3/OOP assaignement 3/File Loading.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_assaignement_3
+{
+    class File_Loading
+    {
+        public static void Loading_Files()
+        {
+            //Loads the file and assaigns it
+            string[] File1 = System.IO.File.ReadAllLines("..\\Net_3_256.txt");
+            string[] File2 = System.IO.File.ReadAllLines("..\\Net_3_256.txt");
+            string Answer = File_Check(File1, File2);
+            if (Answer == "yes")
+            {//prints out if they are the same or not
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Your files are the same!");
+                Console.ReadLine();
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your files are different!");
+            }
+        }
+
+        private static string File_Check(string[] File1, string[] File2)
+        {
+            string Same = "yes";
+            string Different = "no";
+            int i;
+            //checks length of files
+            if (File1.Length != File2.Length)
+            {
+                return (Different);
+            }
+            else
+            {//checks actual characters of files to make sure they are the same
+                for (i = 0; i < File1.Length; i++)
+                {
+                    if (File1[i] != File2[i])
+                    {
+                        return (Different);
+                    }
+                }
+                return (Same);
+                
+            }
+
+        }
+        
+        
+
+    }
+}
---
+++ OOP assaignement 3/OOP assaignement 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP_assaignement_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Answer;
+
+            do
+            {//basic menu
+                Console.WriteLine("Would you like to compare the two files?");
+                Answer = Console.ReadLine();
+                Answer.ToLower();
+                if (Answer == "yes")
+                {
+                    break;
+                }
+                else if (Answer == "no")
+                {
+                    Console.WriteLine("Please close the application");
+                    Console.WriteLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please only enter yes or no.");
+                }
+
+            } while (Answer != "yes" || Answer != "no");
+            File_Loading.Loading_Files();//calls the next function
+        }
+    }
+}
